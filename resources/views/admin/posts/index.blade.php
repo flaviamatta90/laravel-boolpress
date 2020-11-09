479352db9f62f02c43483e9dac9f0b837e7cb24c
Flavia <flaviamatta90@gmail.com>
2020-11-09 17:03:55 +0100

    index view

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,1 +1,23 @@
-@dd($articles);
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Content</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($articles as $article)
+                <tr>
+                    <td>{{$article->title}}}}</td>
+                    <td>{{$article->slug}}}}</td>
+                    <td>{{$article->content}}}}</td>
+                </tr>
+              @endforeach
+          </table>
+    </div>
+@endsection
